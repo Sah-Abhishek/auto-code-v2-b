819c767f0d31b00375f6794eda7b516a125c359c
sah-abhishek <abhisheksah3333@gmail.com>
2026-01-13 13:10:53 +0530

    fixed the conflict

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { pool } from './db/connection.js';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
