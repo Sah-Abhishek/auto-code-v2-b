40f6576ecd5d139602f02baaafae60b998d13668
Admin <admin@brain.com>
2026-01-13 05:10:18 +0000

    updated index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import { pool } from './db/connection.js';
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
